3e2669ae9b9274c719ab34ef9fb110a454cf7143
xXxVladimirxXx <37445491+xXxVladimirxXx@users.noreply.github.com>
2018-08-08 18:00:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 // Front controller
 
     // 1. Общие настройи сайта
-    ini_set('display_errors',1);
-    error_reporting(E_ALL);
-    
     session_start();
 
     // 2. Поключение файлов системы
